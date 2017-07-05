bfe6c3b1ad5622c355c350923c4c75bcbe779ada
orangebaron <orangebaronroblox@gmail.com>
2017-07-05 17:27:12 -0400

    copyright statement seems unnecessary
    copyright statement seems unnecessary for only 4 lines of code

--- terminal/orange_code_terminal_v1.py
+++ terminal/orange_code_terminal_v1.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2017 Sam Alws
-# See <http://www.gnu.org/licenses/> if license is not included with this file.
 from orange_code import orange_code
 myCode = orange_code()
 while True:
